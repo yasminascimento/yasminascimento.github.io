599fdf31366b333cb33c142448def382bd759b9c
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-12-18 16:17:24 -0300

    Create pg.php

---
+++ pg.php
@@ -0,0 +1,89 @@
+<!doctype html>
+<html lang="pt-br">
+<head>
+  <meta charset='utf-8'>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HQS</title>
+  <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css/tema.css">
+</head>
+<body>
+  <nav>
+  <?php include "../nav.php" ?>
+</nav>
+
+  <div class="container text-center">
+    <section>
+      <div class="row">
+      </div>
+  </div>
+  </div>
+  <section class="portfolio section-padding" id="portfolio">
+    <div class="container">
+      <div class="row">
+        <div class="col-md-12">
+          <div class="section-header text-center pb-5">
+            <h2>HQS</h2>
+
+          </div>
+        </div>
+      </div>
+      <section class="portfolio section-padding" id="portfolio">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-6">
+              <div class="card text-light text-center bg-white pb-2">
+                <div class="card-body text-dark">
+                  <div class="img-area mb-3"><img alt="Noticias" class="img-fluid" src="../HQS/img/u1.jpg"></div>
+                  <a href=" HQS/ultimasnoticias.php">
+                    <h3 class="card-title">Noticia</h3>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-6">
+              <div class="card text-light text-center bg-white pb-2">
+                <div class="card-body text-dark">
+                  <div class="img-area mb-3"><img alt="Top Five" class="img-fluid" src="../HQS/img/t1.jpg" height="225px" width="225px"></div>
+                  <a href=" HQS/topfive.php">
+                    <h3 class="card-title">Top Five</h3>
+                  </a>
+                </div>
+              </div>
+            </div>
+      </section>
+      <section class="portfolio section-padding" id="portfolio">
+        <div class="container">
+          <div class="row">
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="card text-light text-center bg-white pb-2">
+                <div class="card-body text-dark">
+                  <div class="img-area mb-3"><img alt="Critica" class="img-fluid" src="../HQS/img/c1.jpg" height="400px" width="400px"></div>
+                  <a href=" HQS/critica.php">
+                    <h3 class="card-title">Critica</h3>
+                  </a>
+                </div>
+              </div>
+            </div>
+            <div class="col-12 col-md-6 col-lg-4">
+              <div class="card text-light text-center bg-white pb-2">
+                <div class="card-body text-dark">
+                  <div class="img-area mb-3"><img alt="Materia" class="img-fluid" src="../HQS/img/m1.jpg" height="225px" width="225px"></div>
+
+                  <a href=" HQS/materia.php">
+                    <h3 class="card-title">Materia</h3>
+                  </a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
+    <footer>
+      <?php include "../rodape.php" ?>
+    </footer>
+</body>
+</html>

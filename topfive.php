3be56268fdc3889e1e2ec66bdc54801afd72e0d0
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-12-18 16:17:43 -0300

    Create topfive.php

---
+++ topfive.php
@@ -0,0 +1,83 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>Topfive</title>
+  <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css/tema.css">
+</head>
+<body>
+  <body>
+    <nav>
+    <?php include "../nav.php" ?>
+</nav>
+
+    <div class="container">
+      <section class="row pt-5 ">
+        <div class="col-12 col-sm-6 col-lg-12 text-center pt-5 ">
+          <img src=" ../HQS/img/t1.jpg" class="img-fluid" alt="TOP1">
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start ">
+          <p>Dias Gigantes começou como uma webcomic, derivada de uma HQ anterior escrita e desenhada por Allison: Scary-Go-Round. Esther fazia parte do elenco coadjuvante da anterior e se tornou uma das protagonistas aqui, e seguiu por um tom diferente, trocando o sobrenatural pelo humor cotidiano.
+            A webcomic evoluiu para uma minissérie em seis edições, da qual os dois últimos capítulos abrem este volume. Eles são melhores que os dois últimos, sobretudo pelas ilustrações de Lissa Treiman.
+            Animadora da Disney (está na equipe de Enrolados, Zootopia e outros), ela entrega um visual detalhado, muito expressivo e deliciosamente cartunesco.
+            Os dois capítulos finais deste volume são os que abrem o título mensal. O ilustrador passou a ser Max Sarin, com desenhos menos sofisticados e inspirados – tanto nas expressões, como nos cenários. Não é um mau trabalho, mas a comparação com a primeira metade do álbum é desfavorável.
+            <a class="text-decoration-none cor-letra" href=" ../autores.php?#renatofelix">
+              <p>Renato Felix,16/08/2021.
+            </a>
+          </p>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 text-center"> <img src=" ../HQS/img/t2.jpg" class="img-fluid" alt="TOP2">
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12  pt-5 pb-5 text-start ">
+          <p> Esta graphic novel é um dos últimos trabalhos de Pazienza, um dos criadores da revista italiana Frigidaire, antes de sua morte. O leitor brasileiro viu algumas de suas HQs curtas na revista Animal, da VHD Diffusion, no final dos anos 1980.
+            Na obra, o leitor encontra o relato das dificuldades enfrentadas por um autor, tanto com as drogas, quanto com não aceitação à crítica ao seu trabalho.
+            Pompeo é um artista amplamente aclamado e procurado para diversos trabalhos, inclusive dava aulas em uma escola de artes. Muitas de suas obras estão por toda a Itália, desde capas de CDs até cartazes de cinema, além das produções em quadrinhos.
+            A narrativa revela um sujeito que não consegue conciliar o vício com a realidade, pois a heroína proporcionava-lhe um universo mais palatável ao que ele ansiava. Pompeo estava sempre às voltas em uma luta consigo mesmo, num escape constante da realidade.
+            Por isso, algumas cenas se repetem em toda a HQ, retratando momentos de consumo excessivo da droga que injetava nas veias alternando com as alucinações em decorrência do efeito da heroína.
+            A arte de Pazienza é graficamente linda, mas sempre carregada com um humor disfórico. Típico de alguém que se caracteriza pela constante irritabilidade, às vezes e carregado de ansiedade.
+            A narrativa se limita a contar a vida mundana e o submundo em que vivia Pompeo. A trama não acrescenta nenhuma informação nova sobre a trajetória e a obra do protagonista, se resumindo em retratar a sua relação com o vício da heroína, causa de sua morte prematura, aos 32 anos.
+            Dessa forma, a história não alcança o mesmo mérito do reconhecimento artístico de Pazienza, e nem o próprio personagem faz jus ao que, na trama, foi sua figura emblemática no cenário cultural italiano. O que se tem é a revelação de uma figura pusilânime sofrendo de uma apatia diante da vida e da sua própria arte.
+            <a class="text-decoration-none cor-letra" href=" ../autores.php?#samirnaliato">
+              <p>Samir Naliato,25/07/2022.</p>
+            </a>
+          </p>
+        </div>
+      </section>
+      <section class="row pt-5 ">
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 text-center"> <img src=" ../HQS/img/t3.jpg" class="img-fluid" alt="TOP3">
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start">
+          <p> Uma experiência pessoal serviu de base para a escrita do roteiro: Mylle Silva fez um intercâmbio no Japão durante seis meses. Na volta, após ter colecionado tantos momentos, sentiu a necessidade de contar essa história.
+            Elisa desembarca no Japão, passa pela experiência de morar em um país tão diferente e ainda consegue construir boas amizades – além de integrar uma banda de Jazz.
+            O roteiro funciona bem, porém é bastante conciso. O leitor poderia conhecer mais Elisa, entender melhor as relações que ela estabeleceu por meio da música, conhecer outros elementos dessa viagem, principalmente as diferenças culturais e dificuldades com o idioma.
+            Tendo isso em vista, vale o questionamento pela opção de um roteiro sem diálogos. A história se beneficiaria muito com textos, que iriam enriquecer o roteiro e poderiam tornar os personagens mais cativantes.
+            A narrativa, porém, é excepcional. Mesmo com a ausência do texto, a arte da Melissa Garabeli conduz o ritmo de leitura de maneira impecável, com páginas duplas lindíssimas, construídas de forma muito orgânica e leve. O traço, apenas com lápis e giz pastel, é bonito e possibilita que o leitor se afeiçoe pela protagonista.
+            <a class="text-decoration-none cor-letra" href=" ../autores.php?#sergiocodespoti">
+              <p>Sergio Codespoti,10/03/2022.</p>
+            </a>
+          </p>
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 text-center"> <img src=" ../HQS/img/t4.jpg" class="img-fluid" alt="TOP4">
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start ">
+          <p> Os Diários de Amora é um quadrinho claramente destinado ao público infantil. Não há nada de mais na história, mas é justamente por sua simplicidade que pode conquistar um leitor mais experiente.
+            Amora tem 10 anos e sonha em ser escritora. O encantamento acontece já nas primeiras páginas do quadrinho, pois a personagem é muito cativante, generosa, corajosa e determinada.
+            É uma história para ler para alguma criança, capaz de aguçar a imaginação de meninas e de meninos, ao se deparar com uma garota sonhadora, inteligente e segura de si. Mas funciona também para adultos que apreciam uma linda trama que remete aos sonhos e mistérios da infância.
+            Impossível não se encantar pela arte. Os traços de Aurélie são delicados e exaltam a singeleza e doçura da protagonista. A escolha da paleta é certeira, as cores acompanham os sentimentos dos personagens.
+            Outra decisão acertada é a intercalação entre quadrinhos e diário, refletindo como uma criança enxerga o mundo, com muitas cores.
+            A edição da Nemo é ótima. Destaque para o letreiramento, se adaptando perfeitamente à arte do quadrinho. A obra, inclusive, ganhou o HQ Mix de Melhor Publicação Infantil de 2018.
+            A série teve início em 2012 e, até o momento, tem seis álbuns, sendo o último publicado em 2018. Infelizmente, até agora, não foi anunciado o lançamento de nenhum outro no Brasil.
+            <a class="text-decoration-none cor-letra" href=" ../autores.php?#samirnaliato">
+              <p>Samir Naliato,17/11/2022.</p>
+            </a>
+          </p>
+        </div>
+      </section>
+    </div>
+    <footer><?php include "../rodape.php" ?></footer>
+  </body>
+</html>

aaa5e4e5fb7296fac312922ebeb98ecdba648282
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-12-18 16:16:37 -0300

    Create critica.php

---
+++ critica.php
@@ -0,0 +1,71 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HQS</title>
+  <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css/tema.css">
+</head>
+
+  <body>
+  <nav>
+    <?php include "../nav.php" ?>
+</nav>
+
+    <div class="container">
+        <section class="row pt-5 ">
+      <div class="col-12 col-sm-6 col-lg-12 text-center pt-5 ">
+          <img src=" ../HQS/img/c1.jpg" class="img-fluid" alt="Raina Telgemeier: um mergulho na adolescência com Drama e Coragem">
+      </div>
+        
+      <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start ">
+          <h1 class="cor-letra">Raina Telgemeier: um mergulho na adolescência com Drama e Coragem</h1>
+          <p>No final de julho deste ano, a quadrinhista Raina Telgemeier (20/05/1977) foi laureada com dois Eisner Awards: Melhor roteirista e artista e Melhor publicação infantil, por Guts, da Scholastic Graphix. Ela tem se destacado no diálogo com o público jovem. Boa parte de seu trabalho é autobiográfico, com foco em aspectos de sua vida na adolescência. Isso, aliado a traços limpos e carismáticos, reforçam a identificação com os leitores. Nos últimos meses, dois de seus álbuns chegaram ao Brasil: Drama (Devir) e Coragem (Intrínseca).
+            Antes, saíram aqui Sorria (lançado originalmente nos Estados Unidos em 2010, seu primeiro álbum como roteirista e desenhista), que a mostrava em um tratamento dentário que complicava sua vida social; e Irmãs (de 2014), sobre o relacionamento com a irmã mais nova – ambos pela Devir.
+            Drama foi publicado originalmente em 2012. A protagonista é Callie, uma garota que trabalha nos cenários das peças de sua escola. A história é sobre sua paixão pelo teatro, enquanto vive seus primeiros amores.
+            Já Coragem, lançado nos EUA em 2019, é um retorno da autora às HQs autobiográficas. Ela fala dos muitos medos que sentia em determinado momento da adolescência, que se refletiam em dores físicas e se revelaram problemas de ansiedade.
+            
+            <a class="text-decoration-none cor-letra" href=" ../autores.php?#pedrohenriqueribeiro">
+              <p>Pedro Henrique Ribeiro,24/09/2022.</p>
+            </a>
+
+            <a class="text-decoration-none cor-letra" href=" ../autores.php?#renatofelix">
+              <p>Renato Felix,08/09/2020.</p>
+            </a>
+          
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 text-center">
+          <img src=" ../HQS/img/c2.jpg" class="img-fluid" alt="Stefan Petrucha: a verdade está nas páginas dos quadrinhos">
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start ">
+          <h1 class="cor-letra">Stefan Petrucha: a verdade está nas páginas dos quadrinhos</h1>
+          <p> Stefan Petrucha é um prolífero autor norte-americano, com trabalhos publicados por grandes editoras lá fora, incluindo romances baseados em super-heróis da Marvel Comics.
+            Esta entrevista foi realizada por e-mail no final de 2018, e trata principalmente pelo trabalho do escritor na série Arquivo X Clássicos, lançada atualmente no Brasil pela New Order Editora.
+            Essas histórias, algumas delas publicadas pela primeira por aqui na década de 1990 pela Myhtos Editora, são consideradas um ponto importante em publicações de franquias licenciadas exatamente por ter um trabalho cuidadoso e apresentar temas e acontecimentos próprios, afastando-se da ideia de serem meras adaptações de episódios. .</p>
+          <a class="text-decoration-none cor-letra" href=" ../autores.php?#renatofelix">
+            <p>Renato Felix,06/09/2020.</p>
+          </a>
+        </div>
+      </section>
+      
+      <section class="row pt-5 ">
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 text-center">
+          <img src=" ../HQS/img/c3.jpg" class="img-fluid" alt="Jeff Smith: Bone e a longa jornada épica do “faça você mesmo”">
+        </div>
+        <div class="col-12 col-sm-6 col-lg-12 pt-5 pb-5 text-start">
+          <h1 class="cor-letra">Jeff Smith: Bone e a longa jornada épica do “faça você mesmo”</h1>
+          <p> No começo dos anos 1990, o título Bone instigou e fomentou o mercado de autopublicação norte-americano numa época em que a toda poderosa indústria dos super-heróis inflava cada vez mais a bolha especulativa com suas sagas caça-níqueis e as coleções de graphic novels – termo criado por Will Eisner (1917-2005) no final dos anos 1970 para “elevar” o status das HQs – estavam crescendo nas prateleiras.
+            Criação do então desconhecido Jeff Smith, um jovem do Meio-Oeste dos Estados Unidos, experiente no campo dos desenhos animados, a longa aventura em preto e branco dos primos Fone Bone, Phoney Bone e Smiley Bone começou em 1991 e terminou em 2004, com 55 edições, dois especiais e mais de 1.300 páginas envolvendo humor, aventura épica e fantasia.
+            A notoriedade alcançada pelo autor no meio indie resultou no reconhecimento traduzido em dezenas de prêmios, dentre eles dez Eisner Awards e 11 Harvey Awards, extrapolando também as fronteiras mercadológicas – tão raras para os independentes –, superando a marca de mais de um milhão de cópias em 15 idiomas ao redor do globo.
+            <a class="text-decoration-none cor-letra" href=" ../autores.php?#sergiocodespoti">
+              <p>Sergio Codespoti,24/09/2019.</p>
+            </a>
+        </div>
+      </section>
+    </div>
+    <footer><?php include "../rodape.php" ?></footer>
+  </body>
+</html>

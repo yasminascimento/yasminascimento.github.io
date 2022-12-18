c762d79aa99852850957447bdd5d6faa674495ea
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-12-18 16:18:03 -0300

    Create ultimasnoticias.php

---
+++ ultimasnoticias.php
@@ -0,0 +1,74 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+  <meta charset="UTF-8">
+  <title>Ultimas noticias</title>
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css/tema.css">
+</head>
+<body>
+  <nav>
+  <?php include "../nav.php" ?>
+</nav>
+
+  <div class="container">
+    <section class="row pt-5 ">
+       <div class="col-12 col-sm-12 col-lg-6 text-center pt-5 ">
+          <img src=" ../HQS/img/u1.jpg" class="img-fluid" alt="Aqui temos a imagem do ultimo desenhão">
+      </div>
+          <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start ">
+        <h1 class="cor-letra">Desenhão: batalha de desenhos ao vivo premiará artista com um iPad Pro</h1>
+        <p class="texto">No próximo sábado, 26 de novembro, a partir das 19h, acontece o Desenhão, uma festa que vai reunir artistas, fãs dos quadrinhos nacionais e amantes da cultura geek no geral numa oportunidade de juntar as pessoas para celebrar, debater assuntos e fazer arte.
+          O atrativo principal da festa é a batalha de desenho. Oito artistas de quadrinhos serão convidados a duelar e desenhar sobre temas sorteados na hora. Eles terão cerca de dois minutos para concluir o desenho e o público decide quem é o(a) vencedor(a), valendo um Ipad Pro na etapa final. Participam Helô D’Angelo, Marilia Marz, Felipe Portugal, Jefferson Costa, Gabú Britto, Flávia Borges, Diego Sanchez e Aline Zouvi.
+          Além da batalha, haverá uma pequena exposição com cartuns e tirinhas sobre autocuidado produzidas pelos oito artistas convidados, a partir de temas sensíveis às marcas parceiras do evento, que são Listerine, Sempre Livre e Carefree.
+          O evento acontecerá na Dona Sonia Bar e Hamburgueria (Rua Júlio Rebollo Perez, 489 - Jardim Peri Peri), em São Paulo/SP, próximo ao Butantã Gibicon, um evento gratuito que tem como objetivo fomentar o cenário das histórias em quadrinhos nacionais e a diversidade de autores brasileiros
+          <a class="text-decoration-none cor-letra" href=" ../autores.php?#samirnaliato">
+            <p>Samir Naliato,2/11/2022.</p>
+          </a>
+        </p>
+      </div>
+      <div class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
+        <img src=" ../HQS/img/u2.jpg" class="img-fluid" alt="Formosa Márcia vence o Prêmio Jabuti na categoria História em Quadrinhos">
+      </div>
+      <div class="col-12 col-sm-12 col-lg-6  pt-5 pb-5 text-start ">
+        <h1 class="cor-letra">Formosa Márcia vence o Prêmio Jabuti na categoria História em Quadrinhos</h1>
+        <p class="texto"> Foi divulgado na data de ontem o vencedor da 64ª edição do Prêmio Jabuti na categoria História em Quadrinhos.
+          O prêmio foi para Escuta, Formosa Márcia, de Marcello Quintanilha (Editora Veneta). Na trama, a enfermeira Márcia, mãe solteira, nascida e criada em uma comunidade do Estado do Rio, vem travando uma verdadeira batalha doméstica para disciplinar sua filha, a insubordinada Jaqueline. Quando a jovem se vê envolvida até o pescoço com o crime organizado, Márcia estará disposta a chegar às últimas consequências para livrá-la dessa enrascada.
+          Confira as demais obras finalistas nesta categoria:
+          A menor distância entre dois pontos é uma fuga (independente), de Gabriel Nascimento e João Henrique Belo;
+          Brega Story (Brasa Editora), de Gidalti Jr.;
+          Manual do Minotauro (Quadrinhos na Cia.), de Laerte;
+          Risca Faca (Monstra), de André Kitagawa.
+          O Jabuti é a mais importante premiação literária do Brasil. O vencedor de cada categoria recebe o valor de R$ 5 mil, e o vencedor do Livro do Ano recebe R$ 100 mil. </p>
+        <a class="text-decoration-none cor-letra" href=" ../autores.php?#marcelonaranjo">
+          <p>Marcelon Aranjo,25/11/2022.</p>
+        </a>
+      </div>
+    </section>
+
+    <section class="row pt-5 ">
+      <div class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
+        <img src=" ../HQS/img/u3.jpg" class="img-fluid" alt="História em quadrinhos sobre Paulo Freire em campanha no Catarse">
+          </div>
+             <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start">
+        <h1 class="cor-letra">História em quadrinhos sobre Paulo Freire em campanha no Catarse</h1>
+        <p class="texto"> A obra busca apresentar o educador a uma nova geração, mas também permitir um reencontro àqueles que conhecem sua história, com quatro narrativas criadas por Rogério Faria (Marighella #LIVRE), Ricardo Sousa (ZéMurai), Jefferson Costa (Jeremias - Pele) e Ren Nolasco (Só Ana), com 20 páginas.
+          As histórias seguem Paulo Freire da infância ao legado nos dias de hoje, destacando-se a experiência de alfabetização em Angicos-RN, a escrita da obra Pedagogia do Oprimido no exílio no Chile, sua contribuição a uma África recém-independente, seu retorno ao país e sua participação no debate sobre políticas públicas de educação, quando chegou a ser secretário de educação no município de São Paulo.
+          De 1930 até os dias atuais, passando por Brasil, Chile, Genebra e Guiné-Bissau, descobrindo um ser humano generoso, que dedicou sua teoria e sua prática para mudar as vidas das pessoas, mudar o mundo.
+          Com prefácio do Padre Júlio Lancellotti e posfácio da professora Eneide Araujo, o projeto foi contemplado pelo ProAC-SP 2021, contando com apoio do Governo do Estado de São Paulo, por meio da Secretaria de Cultura e Economia Criativa.
+          <a class="text-decoration-none cor-letra" href=" ../autores.php?#samirnaliato">
+            <p>Samir Naliato,17/11/2022.</p>
+          </a>
+        </p>
+      </div>
+    </section>
+
+  </div>
+  <footer>
+    <?php include "../rodape.php" ?>
+  </footer>
+</body>
+</html>

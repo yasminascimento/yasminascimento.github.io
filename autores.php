69f6672f05c37cd92ca6bd6c816b8f630aa05e57
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-12-18 16:15:55 -0300

    Create autores.php

---
+++ autores.php
@@ -0,0 +1,84 @@
+<!doctype html>
+<HTML lang="PT-BR">
+<head>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <title>HQS</title>
+  <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css/tema.css">
+</head>
+<body>
+  <nav>
+  <?php include "../nav.php" ?>
+</nav>
+
+  <div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="false">
+    <div class="carousel-indicators">
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+      <button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+    </div>
+    <div class="carousel-inner">
+      <div class="carousel-item active">
+        <img src=" ../HQS/img/a22.jpg" class="d-block w-100" alt="Quadrinhos">
+        <div class="carousel-caption d-none d-md-block">
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src=" ../HQS/img/a23.jpg" class="d-block w-100" alt="Quadrinhos">
+        <div class="carousel-caption d-none d-md-block">
+        </div>
+      </div>
+      <div class="carousel-item">
+        <img src=" ../HQS/img/a24.jpg" class="d-block w-100" alt="Quadrinhos">
+        <div class="carousel-caption d-none d-md-block">
+        </div>
+      </div>
+    </div>
+    <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+      <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Prev</span>
+    </button>
+    <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+      <span class="carousel-control-next-icon" aria-hidden="true"></span>
+      <span class="visually-hidden">Next</span>
+    </button>
+  </div>
+  <div class="container">
+    <section class="row pt-5 ">
+      <div class="col-12 col-sm-12 col-lg-6 text-center pt-5 ">
+        <img src=" ../HQS/img/a1.jpg" class="img-fluid" alt="Mauricio de Sousa">
+      </div>
+      <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start ">
+        <div class="cor-fundo">
+          <h1 class="cor-letra">Mauricio de Sousa</h1>
+          <p>Mauricio de Sousa é desenhista, empresário, o mais premiado autor brasileiro de quadrinhos e membro da Academia Paulista de Letras. Nasceu em 27 de outubro de 1935, numa família de poetas e contadores de histórias em Santa Isabel, no interior de São Paulo. Ainda criança, mudou-se para Mogi das Cruzes, onde descobriu sua paixão pelo desenho e começou a criar os primeiros personagens. Com 19 anos, foi para São Paulo tentar trabalhar como ilustrador na Folha da Manhã (hoje Folha de S.Paulo). No entanto, conseguiu apenas uma vaga de repórter policial.
+            Em 1959, publicou sua primeira tira diária, com as aventuras do garoto Franjinha e do seu cãozinho Bidu. As tiras de Mauricio de Sousa espalharam-se pelos jornais de todo o país, levando-o a montar um estúdio que hoje dá vida a mais de 400 personagens.
+            Em 1970, lançou a revista Mônica e, em 1971, recebeu o mais importante prêmio do mundo dos quadrinhos, o troféu Yellow Kid, em Lucca, na Itália. Seguindo o sucesso de Mônica, outros personagens também ganharam suas próprias revistas, que já passaram pelas editoras Abril e Globo e atualmente estão na Panini.
+            Dos quadrinhos, eles foram para o teatro, o cinema, a televisão, a internet, parques temáticos e até para exposições de arte.
+          </p>
+        </div>
+      </div>
+      <section class="row pt-5 ">
+        <div class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
+          <img src=" ../HQS/img/a2.jpg" class="img-fluid" alt="Katsuhiro Otomo">
+        </div>
+        <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start">
+          <div class="cor-fundo">
+            <h1 class="cor-letra">Katsuhiro Otomo</h1>
+            <p>Mundialmente famoso por ser o autor tanto do mangá quanto da animação de Akira, Katsuhiro Otomo nasceu na província de Miyagi, Japão no ano de 1954.
+              A Gun Report, de 1973, foi sua primeira obra e foi um grande sucesso, se tornando um dos mangás mais vendidos na época. Fireball, lançado em 1979, o consagrou em definitivo como autor de estilo mais ‘cult’. No começo dos anos 1980, Otomo começou a publicar sua obra mais famosa. Akira foi iniciado em 1982 nas páginas da Young Magazine e seria finalizado apenas em 1990, depois do lançamento da versão animada, de 1988, nos cinemas.
+              Depois do sucesso avassalador de Akira, Katsuhiro Otomo lançou Domu, obra essa que lhe rendeu o Grande Prêmio de Ficção Científica Japonês em 1983, feito até então inédito no mundo dos mangás.
+            </p>
+          </div>
+        </div>
+      </section>
+    </section>
+  </div>
+  <footer>
+    <?php include "../rodape.php" ?>
+  </footer>
+</body>
+</html>

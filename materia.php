4baa3a8b2d9aa2b29d397956d38e6bdf40cdd69e
yasminascimento <105990412+yasminascimento@users.noreply.github.com>
2022-12-18 16:17:00 -0300

    Create materia.php

---
+++ materia.php
@@ -0,0 +1,73 @@
+<!doctype html>
+<html lang="en">
+<head>
+  <title>HQS</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1">
+  <link href="../css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-Zenh87qX5JnK2Jl0vWa8Ck2rdkQ2Bzep5IDxbcnCeuOxjzrPF/et3URy9Bv1WTRi" crossorigin="anonymous">
+  <link rel="stylesheet" href="../node_modules/bootstrap-icons/font/bootstrap-icons.css">
+  <link rel="stylesheet" href="../css/tema.css">
+</head>
+<body>
+  <nav>
+  <?php include "../nav.php" ?>
+</nav>
+
+  <div class="container">
+    <section class="row pt-5 ">
+      
+      <div class="col-12 col-sm-12 col-lg-12 text-center pt-5 ">
+        <img src=" ../HQS/img/m1.jpg" class="img-fluid" alt="Guia dos lançamentos de quadrinhos na CCXP22
+        ">
+      </div>
+      <div class="col-12 col-sm-12 col-lg-12 pt-5 pb-5 text-start ">
+        <h1 class="cor-letra">Guia dos lançamentos de quadrinhos na CCXP22</h1>
+        <p>Após três anos, a CCXP volta a ser presencial e é aguardada com ansiedade por fãs e profissionais. O evento ocorre entre 1º e 4 de dezembro e, como sempre, um dos destaques será o Artists' Valley (antigo Artists' Alley), que receberá mais de 482 autores de todo o Brasil e do exterior, divididos em 356 mesas vendendo seus trabalhos. E vários deles serão lançados na CCXP.
+          Diversas editoras também marcarão presença com estandes próprios e no novo espaço Mercado Medieval.
+          Como sempre faz, o Universo HQ apresenta um guia com a lista compilada de tudo que nos foi informado para ajudar você, leitor, a saber algumas das novidades que poderá encontrar na área de exposição da São Paulo Expo. São mais de XXXXXX títulos e quase XXXX imagens dos lançamentos. Use o guia para começar seu planejamento de compras!
+          Os títulos estão separados em ordem de mesas e estandes nos links abaixo. Caso tenha alguma dificuldade, clique no mapa do Artists’ Alley para ampliá-lo e se localizar melhor e visite o site da CCXP para mais informações.
+          Como muitas pessoas pelo Brasil não poderão ir à CCXP, sempre que possível incluímos links de lojas online ou contatos de autores para comprar as publicações pela internet, e assim também aproveitar os materiais.
+          <a class="text-decoration-none cor-letra" href=" ../autores.php?#samirnaliato">
+            <p>Samir Naliato,02/12/2022.</p>
+          </a>
+        </p>
+      </div>
+      <div class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
+        <img src=" ../HQS/img/m2.jpg" class="img-fluid" alt="Do Cruzeiro Novo ao Real: 50 anos de economia brasileira com a revista Mônica">
+      </div>
+      <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start ">
+        <h1 class="cor-letra">Do Cruzeiro Novo ao Real: 50 anos de economia brasileira com a revista Mônica</h1>
+        <p>Comemorando 50 anos em 2020, Mônica é o gibi há mais tempo publicado ininterruptamente no Brasil. São mais de 600 edições, sem deixar de estar nas bancas nem um único mês. A revista possui uma estabilidade que impressiona: praticamente não mudou desde sua estreia, salvo um aumento de 64 para 80 páginas, em 1984.
+          Por isso, por meio de seus preços de capa dá para ter uma ideia da trajetória da economia brasileira no período. Por exemplo, Mônica foi vendida em sete moedas diferentes: Cruzeiro Novo (NCr$), Cruzeiro (Cr$), Cruzado (Cz$), Cruzado Novo (NCz$), novamente Cruzeiro (Cr$), Cruzeiro Real (CR$) e, finalmente, Real (R$).
+          Mônica # 1, em maio de 1970, custou NCr$ 0,90. O Cruzeiro Novo havia sido instituído pela ditadura militar em 1967, substituindo o Cruzeiro, que estava na praça desde 1942. A primeira edição pegou exatamente o último mês da moeda: ainda em maio, ela seria trocada de novo pelo Cruzeiro.
+          Naquele mês, Mônica custava 0,48% do salário mínimo, cujo valor era de NCr$ 187,20. Como o dólar estava cotado a NCr$ 4,53, o gibi custava o equivalente a US$ 0,20.
+        <p>
+          <a class="text-decoration-none cor-letra" href=" ../autores.php?#renatofelix">
+            <p>Renato Felix,29/12/2020.
+          </a>
+        </p>
+      </div>
+    </section>
+    <section class="row pt-5 ">
+      <div class="col-12 col-sm-12 col-lg-6 pt-5 text-center">
+        <img src=" ../HQS/img/m3.jpg" class="img-fluid" alt="Relembrando as HQs do Mestre de Kung Fu">
+      </div>
+      <div class="col-12 col-sm-12 col-lg-6 pt-5 pb-5 text-start">
+        <h1 class="cor-letra">Relembrando as HQs do Mestre de Kung Fu</h1>
+        <p> Shang Chi, o Mestre do Kung Fu, surgiu em dezembro de 1973, nas páginas da revista bimestral Special Marvel Edition # 15, da Marvel Comics.
+          Para colocar o personagem em contexto, as artes marciais haviam se tornado uma mania no mundo inteiro, graças a filmes de Bruce Lee como: Dragão Chinês (The Big Boss), de 1971; A Fúria do Dragão (Fist of Fury), de 1972; O Voo do Dragão (The Way of the Dragon), contracenando com os caratecas Robert Wall e Chuck Norris (cuja luta contra Lee no Coliseu, em Roma, é emblemática), de 1972; e o clássico Operação Dragão (Enter the Dragon), ao lado de Jim Kelly e Bolo Yeung.
+          Além disso, David Carradine fazia o papel de Kwai Chang Caine no seriado Kung Fu, exibido originalmente entre 1972 e 1975. Outro sucesso da televisão era o desenho animado Sawamu – O Demolidor, um animê de 1970, exibido no Brasil pela TV Record, em 1976 – e mais tarde na TV Gazeta – que narrava as aventuras de um lutador de “chute-boxe”.
+          As academias de caratê e kung fu pipocavam por todos os lados.
+          Diante desse cenário, não foi estranho as editoras norte-americanas apostarem nas artes marciais como uma das tendências para atrair novos leitores.
+          <a class="text-decoration-none cor-letra" href=" ../autores.php?#sergiocodespoti">
+            <p>Sergio Codespoti,05/08/2018.
+          </a>
+        </p>
+        </p>
+      </div>
+    </section>
+  </div>
+  <footer><?php include "../rodape.php" ?></footer>
+</body>
+</html>
